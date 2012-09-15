c0f8821a9a680db9ba954ee04c1441efc568fa16
Doc Ritezel <ritezel+github@gmail.com>
2012-09-15 14:34:56 -0700

    clean up after external dependency resolver

--- spec/lib/core/external_spec.sh
+++ spec/lib/core/external_spec.sh
@@ -2,8 +2,16 @@
 
 describe "core/external"
 
+before () {
+  SNOWFLAKE_CACHE=`mktemp -d -t cache`
+}
+
+after () {
+  rm -rf $SNOWFLAKE_CACHE
+}
+
 it_is_able_to_depend_on_a_gist () {
   external gist 3729613
   module depends gist/3729613/sample
-  test -n "`sample test`"
+  test "`sample test`" = "Successfully ran sample test!"
 }
