9140ba205d6f25657b67671dcad7b04cb4e34448
Doc Ritezel <ritezel+github@gmail.com>
2012-09-15 18:04:20 -0700

    add an example of a two-line preamble

---
+++ examples/clobber_example.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+tail -n+`awk '/^__CLOBBER_FILE__/ {print NR + 1; exit 0; }' $0` $0 | snowflake && exit 0
+__CLOBBER_FILE__
+
+module depends core/log
+
+log "okay, i'm in the file."
+log info "here's another line"
